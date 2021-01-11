e30725958e903e19bb120703247bac607e85dff9
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-01-11 19:23:11 +0700

    up no-3

--- server/index.js
+++ server/index.js
@@ -1,48 +1,20 @@
-//  REQUIRE EXPRESSJS
+// REQUIRE
 const express = require('express')
-const path = require('path')
+const usersRoute = require('./routes/users.routes')
 
-const usersRoute = require('./routes/users.route')
-const postsRoute = require('./routes/posts.route')
-const loginRoute = require('./routes/login.route')
-const getFormRoute = require('./routes/getForm.route')
-
-//  ASSIGN APP VARIABLE AND CREATE SERVER
+// ASSIGN
 const app = express()
-const port = 4000
+const port = 3000
 
-//  
+// pug
 app.set('view engine', 'pug')
 app.set('views', './views')
 
-app.use('/', (req, res, next) => {
-  console.log(`The request received at: `, new Date());
-  next()
-})
+// routes
+app.use("/", usersRoute);
 
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.static(path.dirname(__dirname) + '/client/js'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/js/jquery-3.5.1.js', (req, res) => {
-  res.sendFile(path.dirname(__dirname) + '/client/js/jquery-3.5.1.js');
-});
-app.get('/js/jquery-post.js', (req, res) => {
-  res.sendFile(path.dirname(__dirname) + '/client/js/jquery-post.js');
-})
-app.use('/users', usersRoute)
-app.use('/posts', postsRoute)
-app.use('/login', loginRoute)
-app.use('/form', getFormRoute)
-
-app.get('*', (req, res) => {
-  res.send('<h3 style="color:tomato;">404 NOT FOUND!</h3>')
-})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
